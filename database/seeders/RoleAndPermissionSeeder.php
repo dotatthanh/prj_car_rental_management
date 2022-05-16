18abc5f8c2c278a5e2fd42754816c9be7d5babdb
dotatthanh <thanhbuon1910@gmail.com>
2022-05-16 10:53:21 +0700

    fix seed

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -107,5 +107,15 @@ class RoleAndPermissionSeeder extends Seeder
         $admin->givePermissionTo($create_type);
         $admin->givePermissionTo($edit_type);
         $admin->givePermissionTo($delete_type);
+
+        $view_university = Permission::create(['guard_name' => 'admin','name' => 'Xem danh sách trường học']);
+        $create_university = Permission::create(['guard_name' => 'admin','name' => 'Thêm trường học']);
+        $edit_university = Permission::create(['guard_name' => 'admin','name' => 'Chỉnh sửa trường học']);
+        $delete_university = Permission::create(['guard_name' => 'admin','name' => 'Xóa trường học']);
+
+        $admin->givePermissionTo($view_university);
+        $admin->givePermissionTo($create_university);
+        $admin->givePermissionTo($edit_university);
+        $admin->givePermissionTo($delete_university);
     }
 }
